88cc04d5c6c8ef2b8201af56a4720f404dc6518e
Randall A. Gordon <randall@randallagordon.com>
2013-09-29 12:18:49 -0700

    Cleaned up and functional Mocha tests

--- test/users.js
+++ test/users.js
@@ -4,55 +4,48 @@
 var mongoose = require("mongoose");
 var user = require("../lib/users");
 
-mongoose.connect("mongodb://localhost/tdd-test");
+mongoose.connect( "mongodb://localhost/tdd-test" );
 
-describe("User", function(){
+describe( "User", function() {
   var currentUser = null;
 
-  beforeEach(function(done){
-    //add some test data    
-    user.register("test", "password", "test@test.com",  function(doc){
-      currentUser = doc;
+  beforeEach( function( done ) {
+    // Add some test data    
+    user.register( "test", "password", "test@test.com",  function( user ){
+      currentUser = user;
       done();
     });
   });
 
-  afterEach(function(done){
+  afterEach( function( done ){
+    // Nuke the test DB
     user.model.remove({}, function() {
       done();
     });
   });
 
-  it("registers a new user", function(done){
-    user.register("test2", "password2", "test2@test.com", function(doc){
-      console.dir( doc );
-      doc.username.should.equal( "test2" );
-      doc.password.should.not.equal("password2");
-      doc.email.should.equal("test2@test.com");
+  it( "registers a new user", function( done ){
+    user.register( "test2", "password2", "test2@test.com", function( user ){
+      user.username.should.equal( "test2" );
+      user.password.should.not.equal( "password2" );
+      user.email.should.equal( "test2@test.com" );
       done();
-    }, function(message){
-      message.should.equal(null);
-      done();
-    });
-  });
-
-  it("retrieves by email", function(done){
-    user.findByEmail(currentUser.email, function(doc){
-      doc.email.should.equal("test@test.com");
+    }, function( message ){
+      message.should.equal( null );
       done();
     });
   });
 
-  it("retrieves by token", function(done){
-    user.findByToken(currentUser.authToken, function(doc){
-      doc.email.should.equal("test@test.com");
+  it( "retrieves by email", function( done ){
+    user.findByEmail( currentUser.email, function( user ){
+      user.email.should.equal( "test@test.com" );
       done();
     });
   });
 
-  it("authenticates and returns user with valid login", function(done){
-    user.authenticate(currentUser.email, "password", function(user){
-      user.email.should.equal("test@test.com");
+  it( "authenticates and returns user with valid login", function( done ){
+    user.authenticate( currentUser.username, "password", function( user ){
+      user.email.should.equal( "test@test.com" );
       done();
     }, function(){
       throw("oops");
@@ -60,9 +53,9 @@ describe("User", function(){
     });
   });
 
-  it("authenticates and returns fail with invalid login", function(done){
-    user.authenticate(currentUser.email, "liar", function(user){
-      throw("This shouldn't happen");
+  it( "authenticates and returns fail with invalid login", function( done ){
+    user.authenticate( currentUser.username, "liar", function( user ){
+      throw( "This shouldn't happen" );
     }, function(){
       done();
     });
